871eada79f98b4532b6c405400152286cfc62ecf
NeshGogo <rafaelaguero10@hotmail.com>
2021-02-14 23:02:54 -0400

    Agregando mongodb

---
+++ components/message/model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const mySchema = new Schema(
+  {
+    user: String,
+    message: String,
+    date: Date
+  }
+)
+
+const model = mongoose.model('message', mySchema);
+
+module.exports = model;
--- components/message/store.js
+++ components/message/store.js
@@ -1,12 +1,32 @@
-const listMessages = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+db.Promise = global.Promise;
+// creando coneccion;
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'backNode';
+db.connect(`${mongoUrl}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  dbName: dbName,
+})
+  .then(() => console.info('[db] conectada con exito'))
+  .catch((e) => console.error('[db] Error de coneccion', e));
+
+// const listMessages = [];
 
 
 const add = (message) => {
-  listMessages.push(message);
+  //listMessages.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-const list =  () => {
-  return listMessages;
+const list = () => {
+  // return listMessages;
+  return Model.find();
 }
 
 module.exports = {
